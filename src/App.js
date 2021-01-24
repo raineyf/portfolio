81028fcafdd0592381e0c67973e704f41845551a
Rainey F <raineyfleming@protonmail.com>
2021-01-23 21:18:26 -0500

    testing vim prettier autoformatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import './App.scss';
+import "./App.scss";
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <React.Fragment>
-      <Header />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Header />
+		</React.Fragment>
+	);
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
 import React from "react";
-import logo from "../images/RF_Blue-BG.svg"
+import logo from "../images/RF_Blue-BG.svg";
 
 const Header = () => {
 	return (
 		<header>
 			<div className="page-container">
 				<div className="logo-container">
-					<img src={logo} alt="RF Logo" className="logo" />
+					<img
+						src={logo}
+						alt="RF Logo"
+						className="logo"
+					/>
 				</div>
 				<nav>
 					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">About</a></li>
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">About</a>
+						</li>
 					</ul>
 				</nav>
 			</div>
 		</header>
-	)
+	);
 };
 
 export default Header;
