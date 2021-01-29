da7a4ff1b802d546f0476a1ceb7ed572c8041e40
Rainey F <raineyfleming@protonmail.com>
2021-01-28 22:28:14 -0500

    change Header to stateful component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,65 @@
 import React from "react";
 import logo from "../images/RF_Blue-BG.svg";
 
-const Header = () => {
-	return (
-		<header>
-			<div className="page-container">
-				<div className="logo-container">
-					<img
-						src={logo}
-						alt="RF Logo"
-						className="logo"
-					/>
+class Header extends React.Component {
+	constructor() {
+		super();
+		this.state = { ariaExpanded: "false" };
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick() {
+		const ariaExpanded = this.state.ariaExpanded;
+		if (ariaExpanded == "false") {
+			this.setState({ ariaExpanded: "true" });
+		} else {
+			this.setState({ ariaExpanded: "false" });
+		}
+	}
+	render() {
+		return (
+			<header>
+				<div className="page-container">
+					<div className="logo-container">
+						<img
+							src={logo}
+							alt="RF Logo"
+							className="logo"
+						/>
+					</div>
+					<button
+						onClick={this.handleClick}
+						aria-expanded={
+							this.state.ariaExpanded
+						}
+					>
+						Menu
+					</button>
+					<nav
+						className={
+							this.state
+								.ariaExpanded ==
+							"false"
+								? "hidden"
+								: ""
+						}
+					>
+						<ul>
+							<li>
+								<a href="#">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									About
+								</a>
+							</li>
+						</ul>
+					</nav>
 				</div>
-				<nav>
-					<ul>
-						<li>
-							<a href="#">Home</a>
-						</li>
-						<li>
-							<a href="#">About</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</header>
-	);
-};
+			</header>
+		);
+	}
+}
 
 export default Header;
