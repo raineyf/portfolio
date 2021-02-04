e246446c0b319d123a730fb956cc52dd2bddb705
Rainey F <raineyfleming@protonmail.com>
2021-02-03 22:25:24 -0500

    add prevstate to setState function for consistency

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,64 +2,50 @@ import React from "react";
 import logo from "../images/RF_Blue-BG.svg";
 
 class Header extends React.Component {
-	constructor() {
-		super();
-		this.state = { ariaExpanded: "false" };
-		this.handleClick = this.handleClick.bind(this);
-	}
-	handleClick() {
-		const ariaExpanded = this.state.ariaExpanded;
-		if (ariaExpanded == "false") {
-			this.setState({ ariaExpanded: "true" });
-		} else {
-			this.setState({ ariaExpanded: "false" });
-		}
-	}
-	render() {
-		return (
-			<header>
-				<div className="page-container">
-					<div className="logo-container">
-						<img
-							src={logo}
-							alt="RF Logo"
-							className="logo"
-						/>
-					</div>
-					<button
-						onClick={this.handleClick}
-						aria-expanded={
-							this.state.ariaExpanded
-						}
-					>
-						Menu
-					</button>
-					<nav
-						className={
-							this.state
-								.ariaExpanded ==
-							"false"
-								? "hidden"
-								: ""
-						}
-					>
-						<ul>
-							<li>
-								<a href="#">
-									Home
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									About
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</header>
-		);
-	}
+    constructor() {
+        super();
+        this.state = { ariaExpanded: "false" };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        const ariaExpanded = this.state.ariaExpanded;
+        if (ariaExpanded == "false") {
+            this.setState((prevState) => ({ ariaExpanded: "true" }));
+        } else {
+            this.setState((prevState) => ({ ariaExpanded: "false" }));
+        }
+    }
+    render() {
+        return (
+            <header>
+                <div className="page-container">
+                    <div className="logo-container">
+                        <img src={logo} alt="RF Logo" className="logo" />
+                    </div>
+                    <button
+                        onClick={this.handleClick}
+                        aria-expanded={this.state.ariaExpanded}
+                    >
+                        Menu
+                    </button>
+                    <nav
+                        className={
+                            this.state.ariaExpanded == "false" ? "hidden" : ""
+                        }
+                    >
+                        <ul>
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Header;
