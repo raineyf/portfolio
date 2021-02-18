3046f953345af7c7a19f29b03a07cad60a1f6e0c
Rainey F <raineyfleming@protonmail.com>
2021-02-17 20:51:26 -0500

    change Header to functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "../images/RF_Blue-BG.svg";
 
-class Header extends React.Component {
-    constructor() {
-        super();
-        this.state = { ariaExpanded: "false" };
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick() {
-        const ariaExpanded = this.state.ariaExpanded;
+function Header() {
+    const [ariaExpanded, setAriaExpanded] = useState("false");
+    const handleClick = () => {
         if (ariaExpanded == "false") {
-            this.setState((prevState) => ({ ariaExpanded: "true" }));
+            setAriaExpanded("true");
         } else {
-            this.setState((prevState) => ({ ariaExpanded: "false" }));
+            setAriaExpanded("false");
         }
-    }
-    render() {
-        return (
-            <header>
-                <div className="page-container">
-                    <div className="logo-container">
-                        <img src={logo} alt="RF Logo" className="logo" />
-                    </div>
-                    <button
-                        onClick={this.handleClick}
-                        aria-expanded={this.state.ariaExpanded}
-                    >
-                        Menu
-                    </button>
-                    <nav
-                        className={
-                            this.state.ariaExpanded == "false" ? "hidden" : ""
-                        }
-                    >
-                        <ul>
-                            <li>
-                                <Link to="/" onClick={this.handleClick}>
-                                    Home
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/about" onClick={this.handleClick}>
-                                    About
-                                </Link>
-                            </li>
-
-                            <li>
-                                <Link to="/links" onClick={this.handleClick}>
-                                    Links
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
+    };
+    return (
+        <header>
+            <div className="page-container">
+                <div className="logo-container">
+                    <img src={logo} alt="RF Logo" className="logo" />
                 </div>
-            </header>
-        );
-    }
+                <button onClick={handleClick} aria-expanded={ariaExpanded}>
+                    Menu
+                </button>
+                <nav className={ariaExpanded == "false" ? "hidden" : ""}>
+                    <ul>
+                        <li>
+                            <Link to="/" onClick={handleClick}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" onClick={handleClick}>
+                                About
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/links" onClick={handleClick}>
+                                Links
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
